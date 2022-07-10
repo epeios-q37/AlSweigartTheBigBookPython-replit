28a92a50f70ee137ac8b760c43af2b627e64914c
Claude SIMON <epeios-q37@users.noreply.github.com>
2022-07-10 09:09:54 +0200

    Populating

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AlSweigartTheBigBookPython-replit
+# Programs from *The Big Book of Small Python Projects* by Al Sweigart, with a GUI
+
+See <https://q37.info/s/kd3bwchj>.
---
+++ main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[ -d AlSweigartTheBigBookPython ] || git clone https://github.com/epeios-q37/AlSweigartTheBigBookPython
+
+cd AlSweigartTheBigBookPython && python3 replit.py
